f4ccad7c75f21a0b1189ddfdb3111b707bfb1c7d
chiyongs <tlscldyd@gmail.com>
2021-01-18 18:08:05 +0900

    Create pages and routing

--- routes/page.js
+++ routes/page.js
@@ -1,1 +1,17 @@
 // page routing 할 곳
+const express = require("express");
+const router = express.Router();
+
+router.get("/covidStatus", (req, res, next) => {
+  res.render("covidStatus", { title: "WIWY" });
+});
+
+router.get("/city", (req, res, next) => {
+  res.render("city");
+});
+
+router.get("/confirmedMovement", (req, res, next) => {
+  res.render("confirmedMovement");
+});
+
+module.exports = router;
