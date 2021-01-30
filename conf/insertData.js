70895baedcd2e31fcf4d4c2be331eeb038d205d7
chiyongs <tlscldyd@gmail.com>
2021-01-31 01:07:07 +0900

    Replace API connection to nodejs

--- conf/insertData.js
+++ conf/insertData.js
@@ -1,5 +1,8 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var xhr = new XMLHttpRequest();
+// var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+// var xhr = new XMLHttpRequest();
+
+const request = require("request");
+let moment = require("moment");
 var convert = require("xml-js");
 const url =
   "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson"; /*URL*/
@@ -11,34 +14,41 @@ const connection = mysql.createConnection({
   password: PASSWORD,
   database: "WiwyCovid",
 });
-let moment = require("moment");
 
-function updateData() {
+function updateData(fromDate, toDate) {
   const TODAY = moment().format("YYYYMMDD");
   let xml = "";
+
   let queryParams =
     "?" +
     encodeURIComponent("ServiceKey") +
+    "=PPcz55RLIRHgBc%2B5Kzjvbqey%2BsWKrDNmUGNinjzzMcrOygzB%2FI8Tin7bENsGHgDV9puW%2BxpcymvgAU79Rl8S5Q%3D%3D"; /* Service Key*/
+  queryParams +=
+    "&" + encodeURIComponent("pageNo") + "=" + encodeURIComponent("1"); /* */
+  queryParams +=
+    "&" +
+    encodeURIComponent("numOfRows") +
     "=" +
-    "PPcz55RLIRHgBc%2B5Kzjvbqey%2BsWKrDNmUGNinjzzMcrOygzB%2FI8Tin7bENsGHgDV9puW%2BxpcymvgAU79Rl8S5Q%3D%3D"; /*Service Key*/
-  queryParams +=
-    "&" + encodeURIComponent("pageNo") + "=" + encodeURIComponent("1"); /**/
-  queryParams +=
-    "&" + encodeURIComponent("numOfRows") + "=" + encodeURIComponent("10"); /**/
+    encodeURIComponent("10"); /* */
   queryParams +=
     "&" +
     encodeURIComponent("startCreateDt") +
     "=" +
-    encodeURIComponent(TODAY); /**/
+    encodeURIComponent("20210130"); /* */
   queryParams +=
     "&" +
     encodeURIComponent("endCreateDt") +
     "=" +
-    encodeURIComponent(TODAY); /**/
-  xhr.open("GET", url + queryParams);
-  xhr.onreadystatechange = function () {
-    if (this.readyState == 4) {
-      xml = this.responseText;
+    encodeURIComponent("20210130"); /* */
+
+  request(
+    {
+      url: url + queryParams,
+      method: "GET",
+    },
+    function (error, response, body) {
+      if (error) throw error;
+      xml = body;
       if (
         xml ===
         `<?xml version="1.0" encoding="UTF-8" standalone="yes"?><response><header><resultCode>00</resultCode><resultMsg>NORMAL SERVICE.</resultMsg></header><body><items/><numOfRows>10</numOfRows><pageNo>1</pageNo><totalCount>0</totalCount></body></response>`
@@ -51,6 +61,7 @@ function updateData() {
           spaces: 2,
         });
         objResult = JSON.parse(stringResult);
+
         for (let i = 0; i < objResult.response.body.items.item.length; i++) {
           covidSeq = objResult.response.body.items.item[i].seq._text;
           covidGubun = objResult.response.body.items.item[i].gubun._text;
@@ -74,18 +85,94 @@ function updateData() {
           // INSERT QUERY
           connection.query(
             `INSERT INTO Covid202101(seq,gubun,defCnt,incDec,stdDay,gubunCn,gubunEn,qurRate,createDt,deathCnt,updateDt,isolIngCnt,localOccCnt,overFlowCnt,isolClearCnt)
-               values ('${covidSeq}','${covidGubun}', '${covidDefCnt}','${covidIncDec}','${covidStdDay}','${covidGubunCn}','${covidGubunEn}','${covidQurRate}','${covidCreateDt}','${covidDeathCnt}','${covidUpdateDt}','${covidIsolIngCnt}','${covidLocalOccCnt}','${covidOverFlowCnt}','${covidIsolClearCnt}')`,
+         values ('${covidSeq}','${covidGubun}', '${covidDefCnt}','${covidIncDec}','${covidStdDay}','${covidGubunCn}','${covidGubunEn}','${covidQurRate}','${covidCreateDt}','${covidDeathCnt}','${covidUpdateDt}','${covidIsolIngCnt}','${covidLocalOccCnt}','${covidOverFlowCnt}','${covidIsolClearCnt}')`,
             (error, rows, fields) => {
               if (error) throw error;
-              //   console.log("query did work");
+              console.log("query did work");
             }
           );
         } // for loop end
       }
     }
-  };
-
-  xhr.send("");
+  );
 }
+// updateData();
 
 module.exports = updateData;
+
+// function updateData() {
+//   const TODAY = moment().format("YYYYMMDD");
+//   let xml = "";
+//   let queryParams =
+//     "?" +
+//     encodeURIComponent("ServiceKey") +
+//     "=" +
+//     "PPcz55RLIRHgBc%2B5Kzjvbqey%2BsWKrDNmUGNinjzzMcrOygzB%2FI8Tin7bENsGHgDV9puW%2BxpcymvgAU79Rl8S5Q%3D%3D"; /*Service Key*/
+//   queryParams +=
+//     "&" + encodeURIComponent("pageNo") + "=" + encodeURIComponent("1"); /**/
+//   queryParams +=
+//     "&" + encodeURIComponent("numOfRows") + "=" + encodeURIComponent("10"); /**/
+//   queryParams +=
+//     "&" +
+//     encodeURIComponent("startCreateDt") +
+//     "=" +
+//     encodeURIComponent(TODAY); /**/
+//   queryParams +=
+//     "&" +
+//     encodeURIComponent("endCreateDt") +
+//     "=" +
+//     encodeURIComponent(TODAY); /**/
+//   xhr.open("GET", url + queryParams);
+//   xhr.onreadystatechange = function () {
+//     if (this.readyState == 4) {
+//       xml = this.responseText;
+//       if (
+//         xml ===
+//         `<?xml version="1.0" encoding="UTF-8" standalone="yes"?><response><header><resultCode>00</resultCode><resultMsg>NORMAL SERVICE.</resultMsg></header><body><items/><numOfRows>10</numOfRows><pageNo>1</pageNo><totalCount>0</totalCount></body></response>`
+//       ) {
+//         console.log("Did not updated :(");
+//       } else {
+//         console.log("Did update! :)");
+//         const stringResult = convert.xml2json(xml, {
+//           compact: true,
+//           spaces: 2,
+//         });
+//         objResult = JSON.parse(stringResult);
+//         for (let i = 0; i < objResult.response.body.items.item.length; i++) {
+//           covidSeq = objResult.response.body.items.item[i].seq._text;
+//           covidGubun = objResult.response.body.items.item[i].gubun._text;
+//           covidDefCnt = objResult.response.body.items.item[i].defCnt._text;
+//           covidIncDec = objResult.response.body.items.item[i].incDec._text;
+//           covidStdDay = objResult.response.body.items.item[i].stdDay._text;
+//           covidGubunCn = objResult.response.body.items.item[i].gubunCn._text;
+//           covidGubunEn = objResult.response.body.items.item[i].gubunEn._text;
+//           covidQurRate = objResult.response.body.items.item[i].qurRate._text;
+//           covidCreateDt = objResult.response.body.items.item[i].createDt._text;
+//           covidDeathCnt = objResult.response.body.items.item[i].deathCnt._text;
+//           covidUpdateDt = objResult.response.body.items.item[i].updateDt._text;
+//           covidIsolIngCnt =
+//             objResult.response.body.items.item[i].isolIngCnt._text;
+//           covidLocalOccCnt =
+//             objResult.response.body.items.item[i].localOccCnt._text;
+//           covidOverFlowCnt =
+//             objResult.response.body.items.item[i].overFlowCnt._text;
+//           covidIsolClearCnt =
+//             objResult.response.body.items.item[i].isolClearCnt._text;
+//           // INSERT QUERY
+//           connection.query(
+//             `INSERT INTO Covid202101(seq,gubun,defCnt,incDec,stdDay,gubunCn,gubunEn,qurRate,createDt,deathCnt,updateDt,isolIngCnt,localOccCnt,overFlowCnt,isolClearCnt)
+//                values ('${covidSeq}','${covidGubun}', '${covidDefCnt}','${covidIncDec}','${covidStdDay}','${covidGubunCn}','${covidGubunEn}','${covidQurRate}','${covidCreateDt}','${covidDeathCnt}','${covidUpdateDt}','${covidIsolIngCnt}','${covidLocalOccCnt}','${covidOverFlowCnt}','${covidIsolClearCnt}')`,
+//             (error, rows, fields) => {
+//               if (error) throw error;
+//               //   console.log("query did work");
+//             }
+//           );
+//         } // for loop end
+//       }
+//     }
+//   };
+
+//   xhr.send("");
+// }
+
+// module.exports = updateData;
---
+++ conf/test.js
@@ -0,0 +1,28 @@
+var request = require("request");
+
+var url = "http://apis.data.go.kr/1741000/DisasterMsg2/getDisasterMsgList";
+var queryParams =
+  "?" +
+  encodeURIComponent("ServiceKey") +
+  "=" +
+  "PPcz55RLIRHgBc%2B5Kzjvbqey%2BsWKrDNmUGNinjzzMcrOygzB%2FI8Tin7bENsGHgDV9puW%2BxpcymvgAU79Rl8S5Q%3D%3D"; /* Service Key*/
+queryParams +=
+  "&" + encodeURIComponent("pageNo") + "=" + encodeURIComponent("1"); /* */
+queryParams +=
+  "&" + encodeURIComponent("numOfRows") + "=" + encodeURIComponent("10"); /* */
+queryParams +=
+  "&" + encodeURIComponent("type") + "=" + encodeURIComponent("json"); /* */
+queryParams +=
+  "&" + encodeURIComponent("flag") + "=" + encodeURIComponent("Y"); /* */
+
+request(
+  {
+    url: url + queryParams,
+    method: "GET",
+  },
+  function (error, response, body) {
+    console.log("Status", response.statusCode);
+    console.log("Headers", JSON.stringify(response.headers));
+    console.log("Reponse received", body);
+  }
+);
