aca4e99046952d2f826a9ba936c94455cff6bbd0
지현 <57143818+spy03128@users.noreply.github.com>
2021-02-03 00:17:59 +0900

    Modify footer#2

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(
         "'self'",
         "https://cdnjs.cloudflare.com",
         "https://unpkg.com",
+        "'unsafe-inline'",
       ],
       "object-src": ["'none'"],
       "frame-src": ["'self'", "https://www.youtube.com"],
