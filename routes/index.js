e4822008099036e2ad65ef5218eba21638d53ac4
chiyongs <tlscldyd@gmail.com>
2021-02-03 22:45:01 +0900

    Refresh it

--- routes/index.js
+++ routes/index.js
@@ -11,17 +11,18 @@ router.get("/", (req, res, next) => {
   let today = moment().format("YYYYMM");
   const seqNum = calculateSeq();
   const todaySelect = `SELECT * FROM covid${today} WHERE gubun = '합계' and seq >= '${seqNum}'`;
-  dbconn.query(todaySelect, (error, results, fields) => {
-    res.render("index.html", {
-      defCnt: funcConv.defCntConv(results),
-      incDec: funcConv.incDecConv(results),
-      localOccCnt: funcConv.localOccCntConv(results),
-      overFlowCnt: funcConv.overFlowCntConv(results),
-      isolClearCnt: funcConv.isolClearCntConv(results),
-      isolIngCnt: funcConv.isolIngCntConv(results),
-      deathCnt: funcConv.deathCntConv(results),
-    });
-  });
+  // dbconn.query(todaySelect, (error, results, fields) => {
+  //   res.render("index.html", {
+  //     defCnt: funcConv.defCntConv(results),
+  //     incDec: funcConv.incDecConv(results),
+  //     localOccCnt: funcConv.localOccCntConv(results),
+  //     overFlowCnt: funcConv.overFlowCntConv(results),
+  //     isolClearCnt: funcConv.isolClearCntConv(results),
+  //     isolIngCnt: funcConv.isolIngCntConv(results),
+  //     deathCnt: funcConv.deathCntConv(results),
+  //   });
+  // });
+  res.render("index.html", { title: "WIWY" });
 });
 
 module.exports = router;
