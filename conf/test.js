5c91604aa6806a12ab9cbcf2ac34657c20eff8cd
chiyongs <tlscldyd@gmail.com>
2021-02-06 11:59:00 +0900

    Make promise async module for data transaction and give the right data for cities confirmed

--- conf/test.js
+++ conf/test.js
@@ -1,82 +1,101 @@
-const { RFC_2822 } = require("moment");
+// const { RFC_2822 } = require("moment");
 
-const sampleJSON = {
-  body: {
-    items: {
-      item: [
-        {
-          createDt: {
-            _text: "2021-02-0209: 35: 26.009",
-          },
-          deathCnt: {
-            _text: "3",
-          },
-          defCnt: {
-            _text: "2744",
-          },
-          gubun: {
-            _text: "검역",
-          },
-          gubunCn: {
-            _text: "隔離區",
-          },
-          gubunEn: {
-            _text: "Lazaretto",
-          },
-          incDec: {
-            _text: "12",
-          },
-          isolClearCnt: {
-            _text: "2221",
-          },
-          isolIngCnt: {
-            _text: "520",
-          },
-          localOccCnt: {
-            _text: "0",
-          },
-          overFlowCnt: {
-            _text: "12",
-          },
-          qurRate: {
-            _text: "-",
-          },
-          seq: {
-            _text: "7398",
-          },
-          stdDay: {
-            _text: "2021년02월02일00시",
-          },
-          updateDt: {
-            _text: "null",
-          },
-        },
-        {
-          hello: {
-            _text: "hi",
-          },
-        },
-        {
-          buddy: {
-            _text: "budy",
-          },
-          man: {
-            _text: "man",
-          },
-        },
-      ],
-    },
-  },
-};
-const asa = sampleJSON.body;
-for (let i = 0; i < 4; i++) {
-  // console.log(i);
-  // console.log(asa);
-  console.log(i);
-  console.log("sampleLength:", Object.keys(sampleJSON).length);
-  console.log("bodyLength", Object.keys(sampleJSON.body).length);
-  console.log("itemsLength", Object.keys(sampleJSON.body.items).length);
-  console.log("itemLength", Object.keys(sampleJSON.body.items.item).length);
+const funcConv = require("./funcConv");
 
-  // console.log("itemlenght:", sampleJSON.body.items.item[i].length);
+// const sampleJSON = {
+//   body: {
+//     items: {
+//       item: [
+//         {
+//           createDt: {
+//             _text: "2021-02-0209: 35: 26.009",
+//           },
+//           deathCnt: {
+//             _text: "3",
+//           },
+//           defCnt: {
+//             _text: "2744",
+//           },
+//           gubun: {
+//             _text: "검역",
+//           },
+//           gubunCn: {
+//             _text: "隔離區",
+//           },
+//           gubunEn: {
+//             _text: "Lazaretto",
+//           },
+//           incDec: {
+//             _text: "12",
+//           },
+//           isolClearCnt: {
+//             _text: "2221",
+//           },
+//           isolIngCnt: {
+//             _text: "520",
+//           },
+//           localOccCnt: {
+//             _text: "0",
+//           },
+//           overFlowCnt: {
+//             _text: "12",
+//           },
+//           qurRate: {
+//             _text: "-",
+//           },
+//           seq: {
+//             _text: "7398",
+//           },
+//           stdDay: {
+//             _text: "2021년02월02일00시",
+//           },
+//           updateDt: {
+//             _text: "null",
+//           },
+//         },
+//         {
+//           hello: {
+//             _text: "hi",
+//           },
+//         },
+//         {
+//           buddy: {
+//             _text: "budy",
+//           },
+//           man: {
+//             _text: "man",
+//           },
+//         },
+//       ],
+//     },
+//   },
+// };
+// const asa = sampleJSON.body;
+// for (let i = 0; i < 4; i++) {
+//   // console.log(i);
+//   // console.log(asa);
+//   console.log(i);
+//   console.log("sampleLength:", Object.keys(sampleJSON).length);
+//   console.log("bodyLength", Object.keys(sampleJSON.body).length);
+//   console.log("itemsLength", Object.keys(sampleJSON.body.items).length);
+//   console.log("itemLength", Object.keys(sampleJSON.body.items.item).length);
+
+//   // console.log("itemlenght:", sampleJSON.body.items.item[i].length);
+// }
+
+function abc() {
+  console.log("hello");
 }
+
+const promise = new Promise((resolve, reject) => {
+  console.log("hbye");
+});
+
+promise().then(
+  function () {
+    abc();
+  },
+  function (error) {
+    console.error(error);
+  }
+);
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,13 @@ const calToday = require("../conf/calToday");
 const funcConv = require("../conf/funcConv");
 const checkUpdate = require("../conf/checkUpdLog");
 
+function getUpdate(callback) {
+  return new Promise((resolve, reject) => {
+    checkUpdate();
+    resolve();
+  });
+}
+
 /* GET home page. */
 router.get("/", (req, res, next) => {
   let today = moment().format("YYYYMM");
@@ -17,60 +24,58 @@ router.get("/", (req, res, next) => {
   const todaySeq = calToday();
   const todaySelect = `SELECT * FROM covid${today} WHERE gubun = '합계' and seq >= '${todaySeq}'`;
   const citySelect = `SELECT defCnt, incDec FROM covid${today} WHERE seq >= '${todaySeq}' and gubun != '합계'`;
-  checkUpdate();
-  dbconn.query(todaySelect, (error, totalResults, fields) => {
-    dbconn.query(citySelect, (error, cityResults, fields) => {
-      console.log("total:", totalResults);
-      // console.log("city:", cityResults);
-      res.render("index.html", {
-        defCnt: funcConv.defCntConv(totalResults),
-        incDec: funcConv.incDecConv(totalResults),
-        localOccCnt: funcConv.localOccCntConv(totalResults),
-        overFlowCnt: funcConv.overFlowCntConv(totalResults),
-        isolClearCnt: funcConv.isolClearCntConv(totalResults),
-        isolIngCnt: funcConv.isolIngCntConv(totalResults),
-        deathCnt: funcConv.deathCntConv(totalResults),
-        /* 여기서부터 시도별 데이터입니다. Cnt는 누적확진자 수, Inc는 전일 대비입니다. */
-        seoulCnt: funcConv.defCntConv(cityResults),
-        seoulInc: funcConv.incDecConv(cityResults),
-        gyeonggiCnt: funcConv.defCntConv(cityResults),
-        gyeonggiInc: funcConv.incDecConv(cityResults),
-        busanCnt: funcConv.defCntConv(cityResults),
-        busanInc: funcConv.incDecConv(cityResults),
-        daeguCnt: funcConv.defCntConv(cityResults),
-        daeguInc: funcConv.incDecConv(cityResults),
-        incheonCnt: funcConv.defCntConv(cityResults),
-        incheonInc: funcConv.incDecConv(cityResults),
-        gwangjuCnt: funcConv.defCntConv(cityResults),
-        gwangjuInc: funcConv.incDecConv(cityResults),
-        daejeonCnt: funcConv.defCntConv(cityResults),
-        daejeonInc: funcConv.incDecConv(cityResults),
-        ulsanCnt: funcConv.defCntConv(cityResults),
-        ulsanInc: funcConv.incDecConv(cityResults),
-        sejongCnt: funcConv.defCntConv(cityResults),
-        sejongInc: funcConv.incDecConv(cityResults),
-        gangwonCnt: funcConv.defCntConv(cityResults),
-        gangwonInc: funcConv.incDecConv(cityResults),
-        chungcheongbukCnt: funcConv.defCntConv(cityResults),
-        chungcheongbukInc: funcConv.incDecConv(cityResults),
-        chungcheongnamCnt: funcConv.defCntConv(cityResults),
-        chungcheongnamInc: funcConv.incDecConv(cityResults),
-        jeollabukCnt: funcConv.defCntConv(cityResults),
-        jeollabukInc: funcConv.incDecConv(cityResults),
-        jeollanamCnt: funcConv.defCntConv(cityResults),
-        jeollanamInc: funcConv.incDecConv(cityResults),
-        gyeongsangbukCnt: funcConv.defCntConv(cityResults),
-        gyeongsangbukInc: funcConv.incDecConv(cityResults),
-        gyeongsangnamCnt: funcConv.defCntConv(cityResults),
-        gyeongsangnamInc: funcConv.incDecConv(cityResults),
-        jejuCnt: funcConv.defCntConv(cityResults),
-        jejuInc: funcConv.incDecConv(cityResults),
-        lazarettoCnt: funcConv.defCntConv(cityResults),
-        lazarettoInc: funcConv.incDecConv(cityResults),
+  getUpdate().then(() => {
+    dbconn.query(todaySelect, (error, totalResults, fields) => {
+      dbconn.query(citySelect, (error, cityResults, fields) => {
+        res.render("index.html", {
+          defCnt: funcConv.defCntConv(totalResults),
+          incDec: funcConv.incDecConv(totalResults),
+          localOccCnt: funcConv.localOccCntConv(totalResults),
+          overFlowCnt: funcConv.overFlowCntConv(totalResults),
+          isolClearCnt: funcConv.isolClearCntConv(totalResults),
+          isolIngCnt: funcConv.isolIngCntConv(totalResults),
+          deathCnt: funcConv.deathCntConv(totalResults),
+          /* 여기서부터 시도별 데이터입니다. Cnt는 누적확진자 수, Inc는 전일 대비입니다. */
+          seoulCnt: funcConv.defCntConv(cityResults)[0],
+          seoulInc: funcConv.incDecConv(cityResults)[0],
+          busanCnt: funcConv.defCntConv(cityResults)[1],
+          busanInc: funcConv.incDecConv(cityResults)[1],
+          daeguCnt: funcConv.defCntConv(cityResults)[2],
+          daeguInc: funcConv.incDecConv(cityResults)[2],
+          incheonCnt: funcConv.defCntConv(cityResults)[3],
+          incheonInc: funcConv.incDecConv(cityResults)[3],
+          gwangjuCnt: funcConv.defCntConv(cityResults)[4],
+          gwangjuInc: funcConv.incDecConv(cityResults)[4],
+          daejeonCnt: funcConv.defCntConv(cityResults)[5],
+          daejeonInc: funcConv.incDecConv(cityResults)[5],
+          ulsanCnt: funcConv.defCntConv(cityResults)[6],
+          ulsanInc: funcConv.incDecConv(cityResults)[6],
+          sejongCnt: funcConv.defCntConv(cityResults)[7],
+          sejongInc: funcConv.incDecConv(cityResults)[7],
+          gyeonggiCnt: funcConv.defCntConv(cityResults)[8],
+          gyeonggiInc: funcConv.incDecConv(cityResults)[8],
+          gangwonCnt: funcConv.defCntConv(cityResults)[9],
+          gangwonInc: funcConv.incDecConv(cityResults)[9],
+          chungcheongbukCnt: funcConv.defCntConv(cityResults)[10],
+          chungcheongbukInc: funcConv.incDecConv(cityResults)[10],
+          chungcheongnamCnt: funcConv.defCntConv(cityResults)[11],
+          chungcheongnamInc: funcConv.incDecConv(cityResults)[11],
+          jeollabukCnt: funcConv.defCntConv(cityResults)[12],
+          jeollabukInc: funcConv.incDecConv(cityResults)[12],
+          jeollanamCnt: funcConv.defCntConv(cityResults)[13],
+          jeollanamInc: funcConv.incDecConv(cityResults)[13],
+          gyeongsangbukCnt: funcConv.defCntConv(cityResults)[14],
+          gyeongsangbukInc: funcConv.incDecConv(cityResults)[14],
+          gyeongsangnamCnt: funcConv.defCntConv(cityResults)[15],
+          gyeongsangnamInc: funcConv.incDecConv(cityResults)[15],
+          jejuCnt: funcConv.defCntConv(cityResults)[16],
+          jejuInc: funcConv.incDecConv(cityResults)[16],
+          lazarettoCnt: funcConv.defCntConv(cityResults)[17],
+          lazarettoInc: funcConv.incDecConv(cityResults)[17],
+        });
       });
     });
   });
-  // res.render("index.html", { title: "WIWY" });
 });
 
 module.exports = router;
