363cf177277f809d107fdad86b09f4a6ec997b27
chiyongs <tlscldyd@gmail.com>
2021-02-07 00:03:46 +0900

    Redirect if user do not search

--- routes/search.js
+++ routes/search.js
@@ -7,64 +7,16 @@ let client = require("cheerio-httpcli");
 router.post("/search_process", (req, res, next) => {
   const post = req.body;
   const searchTitle = post.searchTitle;
-  //   (searchTitle) => {
-  //     let searchResult = {
-  //       lstOfText: [],
-  //       lstOfLink: [],
-  //     };
-  //     client.fetch(
-  //       "http://www.google.com/search",
-  //       { q: filteredTitle },
-  //       (err, $, response, body) => {
-  //         // console.log(body); -> 제대로 출력이 되는지 확인하기 위해서 작성
-  //         //div yuRUbf의 클래스 안에있는 것들 서치
-  //         let aList = $("div.yuRUbf").find("a");
-  //         for (let i = 0; i < aList.length; i++) {
-  //           searchResult.lstOfText.push($(aList[i]).text());
-  //           searchResult.lstOfLink.push($(aList[i]).attr("href"));
-  //           //   linkList[i] = $(aList[i]).attr("href");
-  //           //   console.log($(aList[i]).text()); //yuRUbf안에 있는 텍스트 출력
-  //           //   console.log($(aList[i]).attr("href")); //링크 출력
-  //         }
-  //         //   return searchResult;
-  //         res.json(searchResult);
-  //       }
-  //     );
-  //   };
-  res.redirect(`/search/${searchTitle}`);
-});
-
-router.get("/searchAPI/:searchTitle", (req, res, next) => {
-  let searchResult = {
-    lstOfText: [],
-    lstOfLink: [],
-  };
-  if (searchResult !== undefined) {
-    client.fetch(
-      "http://www.google.com/search",
-      { q: searchTitle },
-      (err, $, response, body) => {
-        // console.log(body); -> 제대로 출력이 되는지 확인하기 위해서 작성
-        //div yuRUbf의 클래스 안에있는 것들 서치
-        let aList = $("div.yuRUbf").find("a");
-        for (let i = 0; i < aList.length; i++) {
-          searchResult.lstOfText.push($(aList[i]).text());
-          searchResult.lstOfLink.push($(aList[i]).attr("href"));
-          //   linkList[i] = $(aList[i]).attr("href");
-          //   console.log($(aList[i]).text()); //yuRUbf안에 있는 텍스트 출력
-          //   console.log($(aList[i]).attr("href")); //링크 출력
-        }
-        //   return searchResult;
-        res.json(searchResult);
-      }
-    );
+  if (searchTitle) {
+    res.redirect(`/search/${searchTitle}`);
   } else {
     res.redirect("/");
   }
 });
+
 router.get("/:searchTitle", (req, res, next) => {
   const filteredTitle = path.parse(req.params.searchTitle).base;
-
+  console.log(filteredTitle);
   let searchResult = {
     lstOfText: [],
     lstOfLink: [],
@@ -92,8 +44,6 @@ router.get("/:searchTitle", (req, res, next) => {
       });
     }
   );
-  //   console.log(result);
-  //   console.log(searchResult);
-  //   res.send();
 });
+
 module.exports = router;
