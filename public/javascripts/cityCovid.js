b008e0242f7198763c8cd7d56b7a19426b76e3b0
chiyongs <tlscldyd@gmail.com>
2021-02-19 00:35:03 +0900

    Create doughnut chart

--- public/javascripts/cityCovid.js
+++ public/javascripts/cityCovid.js
@@ -1,4 +1,5 @@
 const cityCovidCtx = document.getElementById("cityCovidChart").getContext("2d");
+const cityDnCtx = document.getElementById("citiCovidDn").getContext("2d");
 
 const inpCity = document.getElementById("js-cityCovid");
 
@@ -21,6 +22,58 @@ const cities = [
   "경남",
   "제주",
 ];
+
+function renderDoughnut() {
+  const splitCityData = inpCity.value.split(",");
+  let sortedCityData = [];
+  for (let i = 0; i < splitCityData.length; i++) {
+    let intSplitResult = parseInt(splitCityData[i]);
+    sortedCityData.push(intSplitResult);
+  }
+  sortedCityData.sort((a, b) => {
+    return b - a;
+  });
+  let refinedList = [];
+  let temp = 0;
+  for (let i = 0; i < sortedCityData.length; i++) {
+    if (i < 5) {
+      refinedList.push(sortedCityData[i]);
+    } else {
+      temp = temp + sortedCityData[i];
+    }
+  }
+  refinedList.push(temp);
+  console.log(refinedList);
+  var myChart = new Chart(cityDnCtx, {
+    type: "doughnut",
+    data: {
+      labels: ["서울", "경기", "대구", "인천", "부산", "기타"],
+      datasets: [
+        {
+          label: "도시별 확진자",
+          data: refinedList,
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255,99,132,1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+  });
+}
 function renderChart() {
   const splitCityData = inpCity.value.split(",");
   var myChart = new Chart(cityCovidCtx, {
@@ -68,6 +121,7 @@ function renderChart() {
 
 function init() {
   renderChart();
+  renderDoughnut();
 }
 
 init();
--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,6 @@ router.get("/", (req, res, next) => {
 
   let seqNum = calculateSeq();
   let dailyCovidSql = `SELECT incDec FROM covid${curMonth} WHERE gubun = '합계' and seq >= '${seqNum}'`;
-  let cityCovidSql = `SELECT defCnt FROM covid${curMonth} WHERE seq >= '${seqNum}' and gubun != '합계'`;
   if (checkDay <= "06") {
     let lastMonth = moment().subtract("1", "M").format("YYYYMM");
     dailyCovidSql = `SELECT incDec FROM covid${lastMonth} WHERE gubun = '합계' and seq >= '${seqNum}' UNION SELECT incDec FROM covid${curMonth} WHERE gubun='합계' and seq >= '${seqNum}'`;
@@ -45,82 +44,80 @@ router.get("/", (req, res, next) => {
   let disMsgSelect = `SELECT * FROM dismsg`;
   let diststepSelect = `SELECT * FROM diststep`;
   dbconn.query(dailyCovidSql, (error, dailyResults, fields) => {
-    dbconn.query(cityCovidSql, (error, cityCovidResults, fields) => {
-      dbconn.query(todaySelect, (error, totalResults, fields) => {
-        dbconn.query(citySelect, (error, cityResults, fields) => {
-          dbconn.query(diststepSelect, (error, diststepResults, fields) => {
-            dbconn.query(disMsgSelect, (error, msgResults, fields) => {
-              res.render("index.html", {
-                disMsg: funcConv.msgConv(msgResults),
-                crDate: funcConv.crDateConv(msgResults),
-                locName: funcConv.locNameConv(msgResults),
+    dbconn.query(todaySelect, (error, totalResults, fields) => {
+      dbconn.query(citySelect, (error, cityResults, fields) => {
+        dbconn.query(diststepSelect, (error, diststepResults, fields) => {
+          dbconn.query(disMsgSelect, (error, msgResults, fields) => {
+            res.render("index.html", {
+              disMsg: funcConv.msgConv(msgResults),
+              crDate: funcConv.crDateConv(msgResults),
+              locName: funcConv.locNameConv(msgResults),
 
-                dailyData: funcConv.incDecConv(dailyResults),
-                cityCount: funcConv.defCntConv(cityCovidResults),
-                //
-                seoulStep: funcConv.stepConv(diststepResults)[0],
-                daejeonStep: funcConv.stepConv(diststepResults)[1],
-                ulsanStep: funcConv.stepConv(diststepResults)[2],
-                sejongtStep: funcConv.stepConv(diststepResults)[3],
-                gyeonggiStep: funcConv.stepConv(diststepResults)[4],
-                gangwonStep: funcConv.stepConv(diststepResults)[5],
-                busanStep: funcConv.stepConv(diststepResults)[6],
-                chungcheongbukStep: funcConv.stepConv(diststepResults)[7],
-                chungcheongnamStep: funcConv.stepConv(diststepResults)[8],
-                jeollabukStep: funcConv.stepConv(diststepResults)[9],
-                jeollanamStep: funcConv.stepConv(diststepResults)[10],
-                gyeongsangbukStep: funcConv.stepConv(diststepResults)[11],
-                gyeongsangnamStep: funcConv.stepConv(diststepResults)[12],
-                jejuStep: funcConv.stepConv(diststepResults)[13],
-                daeguStep: funcConv.stepConv(diststepResults)[14],
-                incheonStep: funcConv.stepConv(diststepResults)[15],
-                gwangjuStep: funcConv.stepConv(diststepResults)[16],
-                // disasterMsg: msgObj,
-                defCnt: funcConv.defCntConv(totalResults),
-                incDec: funcConv.incDecConv(totalResults),
-                localOccCnt: funcConv.localOccCntConv(totalResults),
-                overFlowCnt: funcConv.overFlowCntConv(totalResults),
-                isolClearCnt: funcConv.isolClearCntConv(totalResults),
-                isolIngCnt: funcConv.isolIngCntConv(totalResults),
-                deathCnt: funcConv.deathCntConv(totalResults),
-                /* 여기서부터 시도별 데이터입니다. Cnt는 누적확진자 수, Inc는 전일 대비입니다. */
-                seoulCnt: funcConv.defCntConv(cityResults)[0],
-                seoulInc: funcConv.incDecConv(cityResults)[0],
-                busanCnt: funcConv.defCntConv(cityResults)[1],
-                busanInc: funcConv.incDecConv(cityResults)[1],
-                daeguCnt: funcConv.defCntConv(cityResults)[2],
-                daeguInc: funcConv.incDecConv(cityResults)[2],
-                incheonCnt: funcConv.defCntConv(cityResults)[3],
-                incheonInc: funcConv.incDecConv(cityResults)[3],
-                gwangjuCnt: funcConv.defCntConv(cityResults)[4],
-                gwangjuInc: funcConv.incDecConv(cityResults)[4],
-                daejeonCnt: funcConv.defCntConv(cityResults)[5],
-                daejeonInc: funcConv.incDecConv(cityResults)[5],
-                ulsanCnt: funcConv.defCntConv(cityResults)[6],
-                ulsanInc: funcConv.incDecConv(cityResults)[6],
-                sejongCnt: funcConv.defCntConv(cityResults)[7],
-                sejongInc: funcConv.incDecConv(cityResults)[7],
-                gyeonggiCnt: funcConv.defCntConv(cityResults)[8],
-                gyeonggiInc: funcConv.incDecConv(cityResults)[8],
-                gangwonCnt: funcConv.defCntConv(cityResults)[9],
-                gangwonInc: funcConv.incDecConv(cityResults)[9],
-                chungcheongbukCnt: funcConv.defCntConv(cityResults)[10],
-                chungcheongbukInc: funcConv.incDecConv(cityResults)[10],
-                chungcheongnamCnt: funcConv.defCntConv(cityResults)[11],
-                chungcheongnamInc: funcConv.incDecConv(cityResults)[11],
-                jeollabukCnt: funcConv.defCntConv(cityResults)[12],
-                jeollabukInc: funcConv.incDecConv(cityResults)[12],
-                jeollanamCnt: funcConv.defCntConv(cityResults)[13],
-                jeollanamInc: funcConv.incDecConv(cityResults)[13],
-                gyeongsangbukCnt: funcConv.defCntConv(cityResults)[14],
-                gyeongsangbukInc: funcConv.incDecConv(cityResults)[14],
-                gyeongsangnamCnt: funcConv.defCntConv(cityResults)[15],
-                gyeongsangnamInc: funcConv.incDecConv(cityResults)[15],
-                jejuCnt: funcConv.defCntConv(cityResults)[16],
-                jejuInc: funcConv.incDecConv(cityResults)[16],
-                lazarettoCnt: funcConv.defCntConv(cityResults)[17],
-                lazarettoInc: funcConv.incDecConv(cityResults)[17],
-              });
+              dailyData: funcConv.incDecConv(dailyResults),
+              cityCount: funcConv.defCntConv(cityResults),
+              //
+              seoulStep: funcConv.stepConv(diststepResults)[0],
+              daejeonStep: funcConv.stepConv(diststepResults)[1],
+              ulsanStep: funcConv.stepConv(diststepResults)[2],
+              sejongtStep: funcConv.stepConv(diststepResults)[3],
+              gyeonggiStep: funcConv.stepConv(diststepResults)[4],
+              gangwonStep: funcConv.stepConv(diststepResults)[5],
+              busanStep: funcConv.stepConv(diststepResults)[6],
+              chungcheongbukStep: funcConv.stepConv(diststepResults)[7],
+              chungcheongnamStep: funcConv.stepConv(diststepResults)[8],
+              jeollabukStep: funcConv.stepConv(diststepResults)[9],
+              jeollanamStep: funcConv.stepConv(diststepResults)[10],
+              gyeongsangbukStep: funcConv.stepConv(diststepResults)[11],
+              gyeongsangnamStep: funcConv.stepConv(diststepResults)[12],
+              jejuStep: funcConv.stepConv(diststepResults)[13],
+              daeguStep: funcConv.stepConv(diststepResults)[14],
+              incheonStep: funcConv.stepConv(diststepResults)[15],
+              gwangjuStep: funcConv.stepConv(diststepResults)[16],
+              // disasterMsg: msgObj,
+              defCnt: funcConv.defCntConv(totalResults),
+              incDec: funcConv.incDecConv(totalResults),
+              localOccCnt: funcConv.localOccCntConv(totalResults),
+              overFlowCnt: funcConv.overFlowCntConv(totalResults),
+              isolClearCnt: funcConv.isolClearCntConv(totalResults),
+              isolIngCnt: funcConv.isolIngCntConv(totalResults),
+              deathCnt: funcConv.deathCntConv(totalResults),
+              /* 여기서부터 시도별 데이터입니다. Cnt는 누적확진자 수, Inc는 전일 대비입니다. */
+              seoulCnt: funcConv.defCntConv(cityResults)[0],
+              seoulInc: funcConv.incDecConv(cityResults)[0],
+              busanCnt: funcConv.defCntConv(cityResults)[1],
+              busanInc: funcConv.incDecConv(cityResults)[1],
+              daeguCnt: funcConv.defCntConv(cityResults)[2],
+              daeguInc: funcConv.incDecConv(cityResults)[2],
+              incheonCnt: funcConv.defCntConv(cityResults)[3],
+              incheonInc: funcConv.incDecConv(cityResults)[3],
+              gwangjuCnt: funcConv.defCntConv(cityResults)[4],
+              gwangjuInc: funcConv.incDecConv(cityResults)[4],
+              daejeonCnt: funcConv.defCntConv(cityResults)[5],
+              daejeonInc: funcConv.incDecConv(cityResults)[5],
+              ulsanCnt: funcConv.defCntConv(cityResults)[6],
+              ulsanInc: funcConv.incDecConv(cityResults)[6],
+              sejongCnt: funcConv.defCntConv(cityResults)[7],
+              sejongInc: funcConv.incDecConv(cityResults)[7],
+              gyeonggiCnt: funcConv.defCntConv(cityResults)[8],
+              gyeonggiInc: funcConv.incDecConv(cityResults)[8],
+              gangwonCnt: funcConv.defCntConv(cityResults)[9],
+              gangwonInc: funcConv.incDecConv(cityResults)[9],
+              chungcheongbukCnt: funcConv.defCntConv(cityResults)[10],
+              chungcheongbukInc: funcConv.incDecConv(cityResults)[10],
+              chungcheongnamCnt: funcConv.defCntConv(cityResults)[11],
+              chungcheongnamInc: funcConv.incDecConv(cityResults)[11],
+              jeollabukCnt: funcConv.defCntConv(cityResults)[12],
+              jeollabukInc: funcConv.incDecConv(cityResults)[12],
+              jeollanamCnt: funcConv.defCntConv(cityResults)[13],
+              jeollanamInc: funcConv.incDecConv(cityResults)[13],
+              gyeongsangbukCnt: funcConv.defCntConv(cityResults)[14],
+              gyeongsangbukInc: funcConv.incDecConv(cityResults)[14],
+              gyeongsangnamCnt: funcConv.defCntConv(cityResults)[15],
+              gyeongsangnamInc: funcConv.incDecConv(cityResults)[15],
+              jejuCnt: funcConv.defCntConv(cityResults)[16],
+              jejuInc: funcConv.incDecConv(cityResults)[16],
+              lazarettoCnt: funcConv.defCntConv(cityResults)[17],
+              lazarettoInc: funcConv.incDecConv(cityResults)[17],
             });
           });
         });
