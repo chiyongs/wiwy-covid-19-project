841cd94887b610870f4e3122c865df24b55a484e
chiyongs <tlscldyd@gmail.com>
2021-02-23 21:40:48 +0900

    Fix date

--- conf/calToday.js
+++ conf/calToday.js
@@ -1,7 +1,7 @@
 let moment = require("moment");
 function calToday() {
   let TOTALDEFCOUNT = 7152;
-  const GIJOON = "2021-01-20"; // 2021 01 21
+  const GIJOON = "2021-01-19"; // 2021 01 21
   const ONUL = moment().format("YYYY-MM-DD");
   let count = moment(ONUL).diff(moment(GIJOON), "days") * 19;
   if (TOTALDEFCOUNT == 7152) {
